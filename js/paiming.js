c65930beb66590247db7233b8dc332f1c6549246
Your Name <you@example.com>
2017-05-02 22:28:36 +0800

    dfdf

--- js/paiming.js
+++ js/paiming.js
@@ -13,11 +13,19 @@ var query = new AV.Query('Player');
   	return b[0] - a[0];
 	});
 	var k =0;
+	var z= 1;
 	arry.forEach(function () {
 		// body...
 		console.log(arry[k][1]);
+		$('#paiming1').append("<li>"+z+"、"+arry[k][1]+"</li>");
 		k +=1;
+		z +=1;
 	})
 	
     // console.log(arry[1]);
   });
+  $('#btnn').click(function() {
+  	// body...
+  	$('#paiming').css('display','block');
+  });
+  
