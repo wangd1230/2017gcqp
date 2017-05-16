21b13269e20f09c5d79e432066b5061ed9eaaebd
Your Name <you@example.com>
2017-05-16 10:40:35 +0800

    re

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // LeanCloud - 初始化 - 将这里的 APP_ID 和 APP_KEY 替换成自己的应用数据
 // https://leancloud.cn/docs/sdk_setup-js.html#初始化
-var APP_ID = 'L4CStbGN9uWHHFD8mogaacXM-gzGzoHsz';
-var APP_KEY = 'QVpv49ljtHIjataUqDwISHze';
+var APP_ID = 'tKNVoLyeFGNlLQVVbh88b5vi-gzGzoHsz';
+var APP_KEY = 'h6ohBxkGg4mhOMqbRBEsXEgD';
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY
